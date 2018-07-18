a76d83c1c274592e9c39c2f49c204f11f2c76712
YinYichang <yichang1776@163.com>
2018-07-18 16:23:01 +0800

    Initial commit

---
+++ src/com/demo/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+/**
+ * @program: java_demo
+ * @description:
+ * @author: YinYichang
+ * @create: 2018/07/17 16:37
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        /**
+         * 这是一个测试的程序
+         */
+        System.out.println("Hello World;");
+        test();
+    }
+    /** 
+      * @Description:
+      * @Param: [] 
+      * @return: void 
+      * @Author: YinYichang 
+      * @Date: 2018/7/17 
+      */ 
+    public static void test() {
+        int num = 10;
+        for (int i = 1; i < num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + i * j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
